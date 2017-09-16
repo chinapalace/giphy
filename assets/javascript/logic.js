e53489d7d29b091ed649d2c62b391abd5e71f531
Clay Fudurich <clayfudurich@Justins-Macbook.local>
2017-09-16 00:29:49 -0700

    changed http to https for api request

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -53,7 +53,7 @@ function displayGifs() {
       //
       var person = $(this).attr("data-person");
       console.log(person)
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         // ajax call
